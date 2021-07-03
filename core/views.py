262de0f48780e249dc9c8864db41d2d6e22000f8
NachooSilva <ign.silvav@duocuc.cl>
2021-07-02 23:39:22 -0400

    Trabajo finalizado

--- core/views.py
+++ core/views.py
@@ -7,17 +7,30 @@ def inicio(request):
 
 def Galeria(request):
     colab = Colaborador.objects.all()
-    return render(request,'Galeria.html', context={'datos': colab})
+    return render(request,'Galeria.html', context={'datoscolab': colab})
+
+
+
 
 def colaborador_crear(request):
     if request.method == 'POST':
         colaborador_form = ColaboradorForm(request.POST, files=request.FILES)
         if colaborador_form.is_valid():
+            contraseñas = (colaborador_form.cleaned_data['rutColaborador'])[0:2] + ((colaborador_form.cleaned_data['nombre'])[0:2]).upper() + ((colaborador_form.cleaned_data['pais'])[-2:]).lower() + str(colaborador_form.cleaned_data['telefono'])[-2:]
+            rut= colaborador_form.cleaned_data['rutColaborador']
             colaborador_form.save()
+            nuevascontraseñas = Colaborador.objects.get(rutColaborador=rut)
+            nuevascontraseñas.contraseña = contraseñas
+            nuevascontraseñas.save()
+
             return redirect('Galeria')
     else:
         colaborador_form = ColaboradorForm()
     return render(request,'core/colaborador_crear.html', {'colaborador_form': colaborador_form})
+
+
+
+
 
 def colaborador_modi(request, id ):
     colab = Colaborador.objects.get(rutColaborador=id)
